f7bfa8680ad4432a54a3aba8b851c52077c5f09d
zeromake <a390720046@gmail.com>
2019-02-13 14:41:50 +0800

    feat: add more frame parse

---
+++ utils.go
@@ -0,0 +1,28 @@
+package http2
+
+import (
+	"encoding/binary"
+	"io"
+	"sync"
+)
+
+func readByte(p []byte) ([]byte, byte, error) {
+	if len(p) == 0 {
+		return nil, 0, io.ErrUnexpectedEOF
+	}
+	return p[1:], p[0], nil
+}
+
+func readUint32(p []byte) ([]byte, uint32, error) {
+	if len(p) < 4 {
+		return nil, 0, io.ErrUnexpectedEOF
+	}
+	return p[4:], binary.BigEndian.Uint32(p[:4]), nil
+}
+
+var frameCache = &sync.Pool{
+	New: func() interface{} {
+		return make([]byte, FrameHeaderLen)
+	},
+}
+
